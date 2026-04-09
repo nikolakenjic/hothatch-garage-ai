92c2ac6143f27e6c74f2be577a23f3f02ffc731d
Nikola Kenjic <nikola.nico89@gmail.com>
2026-04-09 23:49:20 +0200

    refactor(config): move port configuration to env

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
+import {env} from './config/env.';
 
-const PORT = 5001;
+const PORT = Number(env.PORT);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
