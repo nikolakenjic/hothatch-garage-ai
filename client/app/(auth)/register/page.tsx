1aa2ff1c3b3dc2daec5084d0c59aea3a17214fbb
Nikola Kenjic <nikola.nico89@gmail.com>
2026-05-18 22:49:17 +0200

    feat(auth): add register page with form validation

--- client/app/(auth)/register/page.tsx
+++ client/app/(auth)/register/page.tsx
@@ -1,14 +1,97 @@
+'use client';
+
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {RegisterInput, registerSchema} from '@/lib/validations/auth';
+import {Card, CardContent, CardHeader, CardTitle} from '@/components/ui/card';
+import {Button} from '@/components/ui/button';
+import {Label} from '@/components/ui/label';
+import {Input} from '@/components/ui/input';
+import {registerService} from '@/app/services/auth.service';
+import {useRouter} from 'next/navigation';
+
 export default function RegisterPage() {
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isSubmitting},
+    } = useForm<RegisterInput>({
+        resolver: zodResolver(registerSchema),
+    });
+
+    const onSubmit = async (data: RegisterInput) => {
+        try {
+            const response = await registerService(data);
+            localStorage.setItem('token', response.token);
+            console.log('Register success:', response);
+            router.replace('/login');
+        } catch (error: any) {
+            console.error('Login failed:', error.response?.data?.message);
+        }
+    };
     return (
         <div className="flex min-h-screen items-center justify-center">
-            <div className="w-full max-w-md">
-                <h1 className="text-2xl font-bold text-center mb-6">
-                    Create account
-                </h1>
-                <p className="text-muted-foreground text-center">
-                    Register form coming here
-                </p>
-            </div>
+            <Card className="w-full max-w-md">
+                <CardHeader>
+                    <CardTitle className="text-center">
+                        Create account
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="space-y-4"
+                    >
+                        <div className="space-y-2">
+                            <Label htmlFor="name">Name</Label>
+                            <Input
+                                id="name"
+                                type="text"
+                                {...register('name')}
+                            />
+                            {errors.name && (
+                                <p className="text-sm text-red-500">
+                                    {errors.name.message}
+                                </p>
+                            )}
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                {...register('email')}
+                            />
+                            {errors.email && (
+                                <p className="text-sm text-red-500">
+                                    {errors.email.message}
+                                </p>
+                            )}
+                        </div>
+                        <div className="space-y-2">
+                            <Label htmlFor="password">Password</Label>
+                            <Input
+                                id="input"
+                                type="password"
+                                {...register('password')}
+                            />
+                            {errors.password && (
+                                <p className="text-sm text-red-500">
+                                    {errors.password.message}
+                                </p>
+                            )}
+                        </div>
+                        <Button
+                            type="submit"
+                            className="w-full"
+                            disabled={isSubmitting}
+                        >
+                            {isSubmitting ? 'Creating account...' : 'Register'}
+                        </Button>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     );
 }
